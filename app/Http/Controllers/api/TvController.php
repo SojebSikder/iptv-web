8e4d74aab7a11b9dab56b6ee82a517a06d2d4688
sojebsikder <sojebsikder@gmail.com>
2021-06-19 15:27:13 +0600

    main

---
+++ app/Http/Controllers/api/TvController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tv;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class TvController extends Controller
+{
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api')->only(['store', 'update', 'destroy']);
+    // }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $data = Tv::all();
+        return response()->json(['data' => $data], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = new Tv();
+        $data->title = $request->input('title');
+        $data->link = $request->input('link');
+        $data->is_link_ext = $request->input('is_link_ext');
+        $data->is_image_ext = $request->input('is_image_ext');
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = time() . '-' . uniqid() . '.' . $file->getClientOriginalExtension();
+            $file->move(public_path('uploads/tv'), $filename);
+
+            $data->image = $filename;
+        }
+        $data->status = $request->input('status');
+        // set updated at time
+        $data->updated_at = Carbon::now()->toDateTimeString();
+        $data->save();
+
+        return response()->json(['message' => 'Created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $data = Tv::where('id', $id)->get();
+        return response()->json(['data' => $data], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $data = Tv::where('id', $id);
+        $data->title = $request->input('title');
+        $data->link = $request->input('link');
+        $data->is_link_ext = $request->input('is_link_ext');
+        $data->is_image_ext = $request->input('is_image_ext');
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = time() . '-' . uniqid() . '.' . $file->getClientOriginalExtension();
+            $file->move(public_path('uploads/tv'), $filename);
+
+            $data->image = $filename;
+        }
+        $data->status = $request->input('status');
+        // set updated at time
+        $data->updated_at = Carbon::now()->toDateTimeString();
+        $data->save();
+
+        return response()->json(['data' => $data, 'message' => 'Updated successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $data = Tv::where('id', $id)->first();
+
+        // remove image
+        $this->removeImage($data);
+        $data->delete();
+        return response()->json(['message' => 'Deleted successfully'], 200);
+    }
+    private function removeImage($data)
+    {
+        if ($data->image != "" && !\File::exists('uploads/tv/' . $data->image)) {
+            @unlink(public_path('uploads/tv/' . $data->image));
+        }
+    }
+}
---
+++ database/migrations/2021_06_19_085715_create_tvs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTvsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tvs', function (Blueprint $table) {
+            // $table->id();
+            $table->string('id')->primary()->nullable();
+
+            $table->string('title')->nullable();
+            $table->string('link')->nullable();
+            $table->string('image')->nullable();
+
+            $table->string('is_link_ext')->nullable()->default('false');
+            $table->string('is_image_ext')->nullable()->default('false');
+
+            $table->string('status')->nullable()->default('true');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tvs');
+    }
+}
