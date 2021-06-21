2000994b834fdcc9d490630fa1f081d9ec23d3e0
sojebsikder <sojebsikder@gmail.com>
2021-06-21 22:25:55 +0600

    main

--- app/Http/Controllers/api/TvController.php
+++ app/Http/Controllers/api/TvController.php
@@ -19,7 +19,7 @@ class TvController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
         //
         $data = Tv::with('category')->where('status', "true")->get();
