d01fcab02fc05f28b27acd8bb40cbec560b78b13
Sojeb Sikder <sojebsikder@gmail.com>
2021-10-19 20:25:11 +0600

    main

--- resources/js/app/components/pages/landing/Index.js
+++ resources/js/app/components/pages/landing/Index.js
@@ -14,50 +14,7 @@ import AppFeature from './AppFeature';
 export default function Index() {
     return (
         <div>
-            <Header />
-
-            <section className="home" id="home">
-
-
-                {/* <div className="content">
-                    <h3>{Config.getAppName()}</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus suscipit porro nam libero natus error consequatur sed repudiandae eos quo?</p>
-                    <a href="#about" className="btn">download now</a>
-                </div> */}
-
-                <div className="content">
-                    <h3>Online Pharmacy, Video Consultation With Doctor</h3>
-                    <p>Download and register on our app for free</p>
-                    <a href="#about" className="btn">download now</a>
-                </div>
-
-                <div className="image">
-                    <img src="images/home-img.png" alt="" />
-                </div>
-
-            </section>
-
-
-            {/* App Feature */}
-            {/* <AppFeature /> */}
-
-            {/* About App */}
-            <AboutApp />
-
-            {/* Newsletter */}
-            {/* <Newsletter /> */}
-
-            {/* People Review */}
-            {/* <PeopleReview /> */}
-
-            {/* Pricing plan */}
-            {/* <PricingPlan /> */}
-
-            {/* Contact Us */}
-            {/* <Contact /> */}
-
-            {/* Footer */}
-            <Footer />
+            <h1>Welcome to Choco TV</h1>
         </div>
     )
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>IPTV</title>
+    <title>Choco TV</title>
     <link rel="stylesheet" href=" {{ asset('css/app.css') }}">
 
     <!-- CSS -->
